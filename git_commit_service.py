181625a774f5381798727c48b521f340f567b685
mdotson <mdotson@Helenas-iMac.local>
2014-09-18 19:42:41 -0500

    getting ansible working

--- git_commit_service.py
+++ git_commit_service.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, request, Response
 import json
+from pygit2 import Repository, clone_repository
+from pygit2 import GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE
 
 app = Flask(__name__)
 
@@ -12,19 +14,27 @@ def default():
     if request.method == 'POST':
         try:
             jsonData = request.get_json(force=True)
-            if len(jsonData) != 2:
+            if len(jsonData) != 1:
+                print "hello1"
                 return Response("Invalid JSON. Expecting JSON of the form {\"repo\": \"user/repo\"}", status=400)
             else:
+                print "hello2"
                 repo = str(jsonData['repo'])
-                vcs = str(jsonData['vcs'])
                 splitRepo = repo.split('/')
                 if len(splitRepo) != 2:
                     return Response("Repository must have the form user/repo. Example: mdotson/metrics-dashboard",
                                     status=400)
                 else:
-
-        except Exception:
+                    print "hello4"
+                    try:
+                        the_repo = clone_repository('git://github.com/' + repo + '.git', 'temp_repo')
+                        return str(the_repo)
+                    except Exception as e:
+                        return Response("Repository " + repo + " does not exist.", status=400)
+        except Exception as e:
+            print e.message
             return Response("Invalid JSON. Expecting JSON of the form {\"repo\": \"user/repo\"}", status=400)
-        # repoName = jsonData['repo']
-        # print("hey")
+
+if __name__ == '__main__':
+    app.run()
 
